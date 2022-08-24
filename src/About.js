0f0f8983398e8754da4f3fb02e5cc36758c5099b
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-08-24 17:38:20 +0200

    updating ui

--- src/About.js
+++ src/About.js
@@ -13,9 +13,10 @@ function About() {
                 pokemonAbilities: data.abilities,
                 pokemonSprites0: data.sprites.front_default,
                 pokemonSprites1: data.sprites.front_shiny,
-                pokemonStats: data.stats
+                pokemonStats: data.stats,
+                pokemonType: data.types
             })
-
+            
           
             setMore(poke)
             setFetch(true)
@@ -28,32 +29,31 @@ function About() {
         fetchPokemon()
     }, [])
 
-  
+    console.log(more)
 
     function PokemonData(){
         return (
-            <div className="pokemon-img">
+            <div className="pokemon-card">
+                <h1> {pokemon} </h1>
                 <img src={more.pokemonSprites0}></img>
+                 {more.pokemonType.map((element) => { return <span> {element.type.name}  </span>  })}
                 <h2> Pokemon abilities:</h2>
                 <ul>
-                 
+                    {more.pokemonAbilities.map((element) => {
+                    return <li> {element.ability.name} </li>
+                    })}
                 </ul>
                 <h3> Pokemon stats: </h3>
-                {more.pokemonStats.map(element => {
-                    return <p> {element.base_stat} </p> 
+                {more.pokemonStats.map((element) => {
+                    return <p> {element.stat.name}: {element.base_stat} </p> 
                 })}
             </div>
         )
     }
 
     return (
-        <div className="about">
-            <div className="pokemon">
-                
-                <h1> {pokemon} </h1>
-                {isDataFetched ? PokemonData() : 'Error'}
-                
-            </div>
+        <div className="main">
+            {isDataFetched ? PokemonData() : 'Error'}
         </div>
     )
 }
